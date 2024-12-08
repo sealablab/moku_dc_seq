1a0086f585911f235187fd32a07feb5b6cfabd40
johnycsh <sealablab@gmail.com>
2024-12-07 18:03:14 -0800

    update customwrap formatting

--- moku_customwrap.vhd
+++ moku_customwrap.vhd
@@ -5,15 +5,19 @@ use ieee.numeric_std.all;
 -- Simplifed revision of the CustomWrapper interface 
 entity moku_customwrap
     port (
-        Clk : in std_logic;
-        Reset : in std_logic;
+        Clk : in std_logic; 			-- Clk and Reset
+        Reset : in std_logic;			-- No surprises here
+		--   Inputs (InputA,InputB) 		---
+        InputA : in signed(15 downto 0); --   The Inputs to your custom instrument bitstream. 
+        InputB : in signed(15 downto 0); --   These are always 16-bit ieee_numeric.signed vectors
 
-        InputA : in signed(15 downto 0);
-        InputB : in signed(15 downto 0);
-
-        OutputA : out signed(15 downto 0);
-        OutputB : out signed(15 downto 0);
-
+		--   Outputs (OutputA,OutputB) 		---
+        OutputA : out signed(15 downto 0); --   The Outputs from your custom instrument bitstream. 
+        OutputB : out signed(15 downto 0); --   These are always 16-bit ieee_numeric.signed vectors
+		
+		-- Control Registers  				---
+		-- Every moku instrument bitstream supports ten control registers 
+		-- These are each 32 bit std_logic_vector's
         Control0 : in std_logic_vector(31 downto 0);
         Control1 : in std_logic_vector(31 downto 0);
         Control2 : in std_logic_vector(31 downto 0);
